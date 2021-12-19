ab31af5197b2607ebe86f0e3107fa82a3661cd72
Alan Lin <alin6120404@gmail.com>
2021-12-19 14:38:45 -0800

    adding inquirer to app.js

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [printName, github] = profileDataArgs;
+inquirer
+    .prompt([
+        {
+            type:'input',
+            name:'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const pageHTML = generatePage(printName, github);
   
-  fs.writeFile('index.html', generatePage(printName, github), err => {
-      if(err) throw new Error(err);
+//   fs.writeFile('index.html', pageHTML, err => {
+//       if(err) throw new Error(err);
 
-      console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//       console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
